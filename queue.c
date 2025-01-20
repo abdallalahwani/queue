33d0b05c3a69f4da10fd4f766916225626bedef0
abdalla lahwani <abdalla.lahwani@gmail.com>
2025-01-20 13:22:04 +0200

    last check

--- queue.c
+++ queue.c
@@ -4,29 +4,29 @@
 #include <threads.h>
 #include <stdbool.h>
 
-// Node structure for the queue
+
 struct Node {
     void *data;
     struct Node *next;
 };
 
-// Queue structure with synchronization variables
+
 static struct {
     struct Node *head;
     struct Node *tail;
     size_t count;
-    atomic_size_t visited_count;
+    atomic_size_t visited_count; 
 
-    mtx_t lock;
-    cnd_t not_empty;
-
-    // For FIFO wakeup order of waiting threads
-    size_t next_ticket;
-    size_t current_ticket;
-    size_t wait_count;
+    mtx_t lock;                  
+    cnd_t not_empty;             
+    
+   
+    size_t next_ticket;         
+    size_t current_ticket;      
+    size_t waiters;              
 } queue;
 
-// Initialize the queue
+
 void initQueue(void) {
     queue.head = NULL;
     queue.tail = NULL;
@@ -36,10 +36,10 @@ void initQueue(void) {
     cnd_init(&queue.not_empty);
     queue.next_ticket = 0;
     queue.current_ticket = 0;
-    queue.wait_count = 0;
+    queue.waiters = 0;
 }
 
-// Destroy the queue and clean up resources
+
 void destroyQueue(void) {
     mtx_lock(&queue.lock);
     struct Node *current = queue.head;
@@ -56,7 +56,7 @@ void destroyQueue(void) {
     cnd_destroy(&queue.not_empty);
 }
 
-// Enqueue an item
+
 void enqueue(void *item) {
     struct Node *new_node = malloc(sizeof(struct Node));
     new_node->data = item;
@@ -71,39 +71,25 @@ void enqueue(void *item) {
     queue.tail = new_node;
     queue.count++;
 
-    // Wake all waiting threads to check if they can proceed
-    if (queue.wait_count > 0) {
+ 
+    if (queue.waiters > 0) {
         cnd_broadcast(&queue.not_empty);
     }
     mtx_unlock(&queue.lock);
 }
 
-// Dequeue an item (blocking)
+
 void* dequeue(void) {
     mtx_lock(&queue.lock);
-
-    // Fast path: queue is not empty and no waiters
-    if (queue.count > 0 && queue.wait_count == 0) {
-        struct Node *old_head = queue.head;
-        void *data = old_head->data;
-        queue.head = old_head->next;
-        if (queue.head == NULL) queue.tail = NULL;
-        queue.count--;
-        atomic_fetch_add(&queue.visited_count, 1);
-        free(old_head);
-        mtx_unlock(&queue.lock);
-        return data;
-    }
-
-    // Wait for an item, following FIFO order
     const size_t my_ticket = queue.next_ticket++;
-    queue.wait_count++;
+    queue.waiters++;
 
+    
     while (queue.count == 0 || queue.current_ticket != my_ticket) {
         cnd_wait(&queue.not_empty, &queue.lock);
     }
 
-    // Dequeue the item
+    
     struct Node *old_head = queue.head;
     void *data = old_head->data;
     queue.head = old_head->next;
@@ -112,20 +98,20 @@ void* dequeue(void) {
     atomic_fetch_add(&queue.visited_count, 1);
     free(old_head);
 
-    // Update state for next waiter
+    
     queue.current_ticket++;
-    queue.wait_count--;
-
+    queue.waiters--;
     mtx_unlock(&queue.lock);
     return data;
 }
 
-// Try to dequeue without blocking
+
 bool tryDequeue(void **output) {
     mtx_lock(&queue.lock);
     bool success = false;
 
-    if (queue.count > 0 && queue.wait_count == 0) {
+    
+    if (queue.count > 0 && queue.waiters == 0) {
         struct Node *old_head = queue.head;
         *output = old_head->data;
         queue.head = old_head->next;
@@ -140,7 +126,7 @@ bool tryDequeue(void **output) {
     return success;
 }
 
-// Get the number of visited items (atomic and lock-free)
+
 size_t visited(void) {
     return atomic_load(&queue.visited_count);
 }
